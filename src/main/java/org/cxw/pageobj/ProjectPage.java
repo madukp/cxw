e4a5e612e53f18532bd2f3b66ac12ccb430a5b9b
thiladm <thilani.m@aeturnum.com>
2018-03-22 18:11:35 +0530

    Adding Changes to Project Page

--- src/main/java/org/cxw/pageobj/ProjectPage.java
+++ src/main/java/org/cxw/pageobj/ProjectPage.java
@@ -36,10 +36,61 @@ public class ProjectPage {
 	        }
 		
 		By txtProjectName = By.xpath("//input[@id='projectName' and @name='name']");
-		driver.findElement(txtProjectName).click();
+		driver.findElement(txtProjectName).sendKeys("TestProject_AUT");
 		
-		By txtProjectLeader = By.xpath("//div[@id='projectLeader' and @name='projectLeader']");
-		driver.findElement(txtProjectName).click();
+		
+		By drpdwnProjectLeader = By.xpath("//div[@id='projectLeader' and @name='projectLeader']");
+		driver.findElement(drpdwnProjectLeader).click();
+		
+		By selectProjectLeader = By.xpath("//span[contains(text(),'Ann Brown')]");
+		driver.findElement(selectProjectLeader).click();
+		
+		By collapseProject = By.xpath("//div[@class='collapse-btn pull-right' and @ng-click='tab[1] = !tab[1]']");
+		driver.findElement(collapseProject).click();
+		
+		try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
+		
+		By expandMap = By.xpath("//div[@class='collapse-btn pull-right' and @ng-click='tab[2] = !tab[2]']");
+		driver.findElement(expandMap).click();
+		
+		try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
+		
+		By drpdwnSelectTemplate = By.xpath("//div[@id='stageTemplate' and @name='stageTemplate']");
+		driver.findElement(drpdwnSelectTemplate).click();
+		
+		By SelectTemplate = By.xpath("//span[@class='ng-binding ng-scope' and contains(text(),'SPCI Template')]");
+		driver.findElement(SelectTemplate).click();
+		
+		try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
+		
+		//WebElement chkbxChannel = driver.findElement(By.xpath("//input[@class='ng-valid ng-dirty ng-valid-parse ng-touched']"));
+		//System.out.println(chkbxChannel.getText());
+		//chkbxChannel.click();
+		
+		By radiobtnCurvetype = By.xpath("//button[@id='curveTypeE' and contains(text(), 'Emotion')]");
+		driver.findElement(radiobtnCurvetype).click();
+		
+		By btnChooseImage = By.xpath("//button[@class='btn-sm btn btn-default ng-scope' and contains(text(), 'Choose File')]");
+		driver.findElement(btnChooseImage).click();
+		
+		try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
+		
+		
+		
+		
+		
 		
 		
 		
