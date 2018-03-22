4a4fe2b3edea3c911af52ca20fd69d8cc0faf3bb
thiladm <thilani.m@aeturnum.com>
2018-03-22 16:16:47 +0530

    Adding Project Page

--- src/main/java/org/cxw/pageobj/ProjectPage.java
+++ src/main/java/org/cxw/pageobj/ProjectPage.java
@@ -14,10 +14,35 @@ public class ProjectPage {
 	
 	//navigate to CX project
 		
+		   try {
+	            Thread.sleep(5000);
+	        } catch (InterruptedException e) {
+	        }
+		
 		By MenuItemCXProject = By.xpath("//a[@href='#/project' and contains(text(),'CX Projects')]");
 		driver.findElement(MenuItemCXProject).click();
-				
-	
+		
+		 try {
+	            Thread.sleep(2000);
+	        } catch (InterruptedException e) {
+	        }
+		
+		By btnNewCXProject = By.xpath("//button[@class='btn create-btn btn-primary' and contains(text(),'Create New Project')]");
+		driver.findElement(btnNewCXProject).click();
+		
+		 try {
+	            Thread.sleep(1000);
+	        } catch (InterruptedException e) {
+	        }
+		
+		By txtProjectName = By.xpath("//input[@id='projectName' and @name='name']");
+		driver.findElement(txtProjectName).click();
+		
+		By txtProjectLeader = By.xpath("//div[@id='projectLeader' and @name='projectLeader']");
+		driver.findElement(txtProjectName).click();
+		
+		
+		
 	}
 
 }
